c0fc4a4c0249b6401884f75d76c60c725d960789
MuhammadSyaugi13 <muhammadsyaugi13@gmail.com>
2022-10-27 18:10:20 +0700

    awal init

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hello works</title>
+</head>
+<body>
+	<h1>its work</h1>
+	<h3>Hello my name is {{$name}}</h3>
+</body>
+</html>
